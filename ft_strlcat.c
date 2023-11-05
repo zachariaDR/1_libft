29c8fcb9cc5dbcd31a98dcdd2ead76b8f8127b08
Zakaria Adriouche <zachichigo20@gmail.com>
2023-11-05 18:41:25 +0100

    some change

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,19 +1,25 @@
-size_t	strlcat(char *dst, const char *src, size_t size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	size_t i;
-	size_t j;
-	
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dlen;
+	unsigned int	slen;
+
 	i = 0;
 	j = 0;
-	while (dst[i] && i < size)
-		i++;
-	tmp = i;
-	while (src[j] && (tmp + j + 1) < size)
+	while (dest[j] != '\0')
 	{
-		dst[i + j] = src[j];
+		j++;
+	}
+	dlen = j;
+	slen = ft_strlen(src);
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
 		i++;
 		j++;
 	}
-	if (i < size)
-		dst[i + j] = 0;
-	return x:
+	dest[j] = '\0';
+	return (dlen + slen);
